25054ea830c3cd9ec670b0c88dbc1e2b1b6664b6
cristian15786 <cristianbenitezg06@gmail.com>
2022-11-03 11:33:42 -0500

    confirmacion de modificaciones

---
+++ js/menu subitems.js
@@ -0,0 +1,33 @@
+
+let botonMenu = document.getElementById("botonMenu");
+
+botonMenu.addEventListener("click", function(){
+    alert("¡NO ME TOQUE!");
+}
+
+);
+
+let botonNosotros = document.getElementById("botonNosotros");
+
+botonNosotros.addEventListener("click", function(){
+    alert("¡NO ME TOQUE!");
+}
+
+);
+
+let botonContactenos = document.getElementById("botonContactenos");
+
+botonContactenos.addEventListener("click", function(){
+    alert("¡NO ME TOQUE!");
+}
+
+);
+
+let botonGaleria = document.getElementById("botonGaleria");
+
+botonGaleria.addEventListener("click", function(){
+    alert("¡NO ME TOQUE!");
+}
+
+);
+
---
+++ js/script.js
@@ -0,0 +1,3 @@
+alert("este es mi primer mensaje en javascript"); 
+/* Variable declarada pero no inicializada */
+let botonMenu
